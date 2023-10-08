e91e23528b03e9cdd41c4d545f84a0f858665588
rmens <r.mens@me.com>
2023-10-08 23:49:34 +0200

    Update options-page.php

--- options-page.php
+++ options-page.php
@@ -115,6 +115,7 @@ class TekstTVGPT_OptionsPage {
             '<input type="number" id="word_limit" name="ttvgpt_word_limit" value="%s" min="1" />',
             esc_attr(get_option('ttvgpt_word_limit', 100))
         );
+        echo '<p class="description">GPT models are not very good at respecting the amount of words. This is only an indicator, not a hard limit.</p>';
     }
 
     public function model_callback() {
