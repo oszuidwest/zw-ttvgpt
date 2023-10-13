e5a370d7f737b6da4a25b4ee32f0c7242ff21ecb
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2023-10-13 22:27:59 +0200

    Re-add main file

---
+++ ttvgpt.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Tekst TV GPT
+Description: Maakt met OpenAI's GPT een samenvatting van een artikel voor op Tekst TV en plaatst dit in het juiste ACF-veld
+Version: 0.2
+Author: Raymon Mens
+*/
+
+use ZuidWest\TekstTVGPT\OptionsPage;
+use ZuidWest\TekstTVGPT\Plugin;
+
+require_once 'src/OptionsPage.php';
+require_once 'src/Plugin.php';
+
+if (is_admin()) {
+    new OptionsPage();
+}
+
+new Plugin();
