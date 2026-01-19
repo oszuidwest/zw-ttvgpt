ebbcec41ed6bf1a713f01023ea7d7f02cc89ba03
rmens <r.mens@me.com>
2026-01-19 23:53:52 +0100

    fix: add ABSPATH check to phpstan-bootstrap.php

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -5,6 +5,10 @@
  * @package ZW_TTVGPT
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
 define( 'ZW_TTVGPT_VERSION', '1.0.0' );
 define( 'ZW_TTVGPT_DIR', __DIR__ . '/' );
 define( 'ZW_TTVGPT_URL', 'https://example.com/wp-content/plugins/zw-ttvgpt/' );
