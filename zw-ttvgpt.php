435fb9bbee60acb2880fabdddd4a9c1218e4c1a2
rmens <r.mens@me.com>
2026-01-22 01:30:06 +0100

    chore: bump version to 0.2.1-beta.3

--- zw-ttvgpt.php
+++ zw-ttvgpt.php
@@ -3,7 +3,7 @@
  * Plugin Name: ZuidWest Tekst TV GPT
  * Plugin URI: https://github.com/oszuidwest/zw-ttvgpt
  * Description: Genereert automatisch samenvattingen voor Tekst TV met behulp van OpenAI GPT-modellen
- * Version: 0.2.1-beta.2
+ * Version: 0.2.1-beta.3
  * Author: Streekomroep ZuidWest
  * Author URI: https://www.zuidwesttv.nl
  * License: GPL-2.0-or-later
