63bb4b48d4518d765276910aa9c8221fee9051a2
Raymon Mens <raymon@zuidwestfm.nl>
2026-05-14 19:15:53 +0200

    chore: bump version to 0.4.0

--- zw-ttvgpt.php
+++ zw-ttvgpt.php
@@ -3,7 +3,7 @@
  * Plugin Name: ZuidWest Tekst TV GPT
  * Plugin URI: https://github.com/oszuidwest/zw-ttvgpt
  * Description: Genereert automatisch samenvattingen voor Tekst TV met behulp van OpenAI GPT-modellen
- * Version: 0.3.1
+ * Version: 0.4.0
  * Author: Streekomroep ZuidWest
  * Author URI: https://www.zuidwesttv.nl
  * License: GPL-3.0-or-later
